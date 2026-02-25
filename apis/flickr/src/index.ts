f1dabd1e18cfdb8fcdc0ec020ed1a2d78e9510f9
Dmytro Zelenetskyi <dmitry.zelenetskiy@gmail.com>
2026-02-25 08:46:05 +0100

    Update figma api spec

--- apis/flickr/src/index.ts
+++ apis/flickr/src/index.ts
@@ -2,15 +2,10 @@ import * as photosets from "./api/photosets.js";
 import * as activity from "./api/activity.js";
 import { query, createBuildEndpointFn } from "@zemd/http-client";
 
-export const flickr = (
-  apiKey: string,
-  opts?: { url?: string; debug?: boolean },
-) => {
+export const flickr = (apiKey: string, opts?: { url?: string; debug?: boolean }) => {
   const build = createBuildEndpointFn({
     baseUrl: opts?.url ?? "https://api.flickr.com/services/rest",
-    transformers: [
-      query({ api_key: apiKey, format: "json", nojsoncallback: 1 }),
-    ],
+    transformers: [query({ api_key: apiKey, format: "json", nojsoncallback: 1 })],
     debug: !!opts?.debug,
   });
 
